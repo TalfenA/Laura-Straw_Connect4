c04f5b99df8c52820e6848d3356a8bbbf9d0cfcc
Laura Straw <lauratalfen@duck.com>
2026-04-27 20:08:49 +0100

    Remove incorrect version upload

--- Documentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Board"},{"p":"org.example","l":"Game"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Player"}];updateSearchResults();
